915483d36da84778663390384fc127cc5631765e
Matthew McDonald <matt.mcodnald21@gmail.com>
2018-08-16 18:01:57 -0500

    just showing someone my old portfolio

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
 const nav = document.querySelector('.nav');
 const row = document.querySelector('.row');
 const mission = document.querySelector('.mission');
+const about = document.querySelector('.about');
 const down = document.getElementById('down');
 
 
@@ -17,6 +18,7 @@ function openNav() {
     nav.style.display = "block";
     row.style.display = "none";
     mission.style.display = "none";
+    about.style.display = "none";
 }
 
 /* Close */
@@ -24,9 +26,12 @@ function closeNav() {
     nav.style.display = "none";
     row.style.display = "block";
     mission.style.display = "block";
+    about.style.display = "block";
 }
 
-function getIt() {
+
+
+function aboutMe() {
 	var down = document.querySelector('#down');
 
 	document.getElementById('about').scrollIntoView({behavior: "smooth",block: "start", inline: "start"});
---
+++ js/index2.js
@@ -0,0 +1,15 @@
+function find() {
+	var pro = document.getElementById('projects');
+	pro.scrollIntoView();
+};
+
+function myFunction() {
+    var link = document.getElementById("links");
+    link.scrollIntoView();
+}
+
+
+function hello(){
+var name = prompt('what is your name?');
+alert('hello '+ name + '!!!! Thank you for stopping by!!');
+}
